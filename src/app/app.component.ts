6e34439e3fc50e8d54b8bb16848ed2aac6fdcc7c
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-15 15:09:34 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'AutuUI_Devtest_151508';
+}
